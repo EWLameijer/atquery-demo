78d4f1fbca8473bedd8dea173637b80f565eafca
Eric-Wubbo Lameijer <eric_wubbo@hotmail.com>
2024-05-18 12:21:01 +0200

    Method 1: overriding repository methods and delegating

--- src/main/java/org/ericwubbo/atquerydemo/ItemRepository.java
+++ src/main/java/org/ericwubbo/atquerydemo/ItemRepository.java
@@ -1,8 +1,28 @@
 package org.ericwubbo.atquerydemo;
 
+import jakarta.annotation.Nonnull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNullApi;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    @Override
+    @Nonnull
+    default List<Item> findAll() {
+        return findByHasBeenDeleted(false);
+    }
+
+    @Override
+    default void deleteById(@Nonnull Long id) {
+        Optional<Item> possibleItem = findById(id);
+        if (possibleItem.isEmpty()) throw new IllegalArgumentException("Item with id " + id + " does not exist!");
+        Item item = possibleItem.get();
+        item.setHasBeenDeleted(true);
+        save(item);
+    }
+
+    List<Item> findByHasBeenDeleted(boolean b);
 }
