eae8cab36959e0418a8182ad1b07f70f088391e1
Eric-Wubbo Lameijer <eric_wubbo@hotmail.com>
2024-05-18 13:24:53 +0200

    adding the delete

--- src/main/java/org/ericwubbo/atquerydemo/ItemRepository.java
+++ src/main/java/org/ericwubbo/atquerydemo/ItemRepository.java
@@ -1,12 +1,18 @@
 package org.ericwubbo.atquerydemo;
 
+import jakarta.annotation.Nonnull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
-
     @Query("select e from #{#entityName} e where e.deleted=false")
     List<Item> findAllPurchasable();
+
+    @Override
+    @Modifying
+    @Query("update #{#entityName} set deleted=true where id=?1")
+    void deleteById(@Nonnull Long id);
 }
